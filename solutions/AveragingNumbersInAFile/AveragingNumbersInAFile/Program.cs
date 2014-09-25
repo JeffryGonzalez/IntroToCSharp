41acd36d1c3a8fdc5bab8a2d8f7ccaf1797c2219
Jeffry Gonzalez <jeff@hypertheory.com>
2014-09-25 12:16:34 -0400

    Done with most of third challenge

---
+++ solutions/AveragingNumbersInAFile/AveragingNumbersInAFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AveragingNumbersInAFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string fileName = @"c:\practices\numbers.txt";
+			var numbers = new List<double>();
+
+			if (File.Exists(fileName))
+			{
+				var f = File.OpenText(fileName);
+				while (f.Peek() >= 0)
+				{
+					numbers.Add(double.Parse(f.ReadLine()));
+				}
+
+				Console.WriteLine("The average of all the number is {0}", numbers.Average());
+			}
+			else
+			{
+				Console.WriteLine(@"The File c:\practices\numbers.txt does not exit");
+			}
+		}
+	}
+}
